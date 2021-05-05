da5d42d44aa8efe420924489c2b900ac02dc468c
bjh3311 <bjh3311@naver.com>
2021-05-05 17:43:01 +0900

    flip으로 캐릭터 뒤집기성공

--- New Unity Project/Assets/Scenes/Free Pixel Army/Scripts/Player.cs
+++ New Unity Project/Assets/Scenes/Free Pixel Army/Scripts/Player.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    SpriteRenderer rend;
     public Vector2 speed_vec;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<SpriteRenderer>();
     }
     // Update is called once per frame , 1분에 약60번 업데이트
     void Update()
@@ -16,10 +17,12 @@ public class Player : MonoBehaviour
         speed_vec = Vector2.zero;//1초마다 계속 0으로 업데이트
         if(Input.GetKey(KeyCode.RightArrow))//오른쪽 방향키 누르면
         {
+            rend.flipX = false;//오른쪽이면 뒤집지 않는다
             speed_vec.x += 0.1f;
         }
         if(Input.GetKey(KeyCode.LeftArrow))//왼쪽 방향키 누르면
         {
+            rend.flipX = true;//왼쪽이면 뒤집는다
             speed_vec.x += -0.1f;
         }
         if(Input.GetKey(KeyCode.Space))//스페이스바 누르면
---
+++ New Unity Project/Assets/Scenes/Free Pixel Army/Scripts/Weapon.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    SpriteRenderer rend;
+    public Vector2 speed_vec;
+    public Vector2 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame , 1분에 약60번 업데이트
+    void Update()
+    {
+        Vector2 position = gameObject.transform.localPosition;
+        speed_vec = Vector2.zero;//1초마다 계속 0으로 업데이트
+        if (Input.GetKey(KeyCode.RightArrow))//오른쪽 방향키 누르면
+        {
+            if(rend.flipX)//원래 왼쪽을 보고있었다면
+            {
+                position.x = position.x + 0.205f;//2.05는 캐릭터의 가로 크기
+                transform.position = new Vector2(position.x, position.y);
+            }
+            rend.flipX = false;//오른쪽이면 뒤집지 않는다
+            speed_vec.x += 0.1f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))//왼쪽 방향키 누르면
+        {
+            if (rend.flipX)//원래 왼쪽을 보고있었다면
+            {
+                position.x = position.x - 0.205f;//2.05는 캐릭터의 가로 크기
+                transform.position = new Vector2(position.x, position.y);
+
+            }
+            rend.flipX = true;//왼쪽이면 뒤집는다
+            speed_vec.x += -0.1f;
+        }
+        if (Input.GetKey(KeyCode.Space))//스페이스바 누르면
+        {
+            speed_vec.y += 0.1f;
+        }
+        transform.Translate(speed_vec);
+    }
+}
