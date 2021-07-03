ebdbe587d8097c8acc37859557e2eb8aa6668f6d
bjh3311 <bjh3311@naver.com>
2021-07-03 20:02:30 +0900

    FSM 구현중

---
+++ New Unity Project/Assets/Scenes/Free Pixel Army/Scripts/Boss_FSM.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Boss_Fsm : MonoBehaviour
+{
+    public enum CurrentState { idle,attack,walk};
+    public CurrentState curState = CurrentState.idle;//초기상태는 idle
+    private Transform _transform;
+    private Transform _player_transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = this.gameObject.GetComponent<Transform>();
+        _player_transform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        StartCoroutine(this.CheckState());//현재상태를 체크하는 코루틴 시작
+    }
+
+    IEnumerator CheckState()
+    {
+        yield return new WaitForSeconds(0.2f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
